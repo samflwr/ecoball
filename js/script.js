65425aa471ed1d01b709df0fc6ad9f6d31c32eca
samflwr <109246278+samflwr@users.noreply.github.com>
2022-10-02 00:49:12 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ let btn = document.getElementById('btn')
 let viento = document.getElementById('viento')
 
 
+
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
     
@@ -16,8 +17,12 @@ window.addEventListener('scroll', function(){
     nube1.style.left = value* -5  + 'px';
     nube2.style.left = value* -5  + 'px';
     btn.style.marginTop = value * 0.7 + 'px';
-    slogan.style.marginBottom =   value* -1  + 'px';
-    subslogan.style.marginBottom =   value* -1  + 'px';
+    slogan.style.marginTop = value*1.3  + 'px';
+    slogan.style.opacity =   100+value* -0.07  + '%';
+    subslogan.style.marginBottom =   value* -1.6  + 'px';
+    subslogan.style.opacity =   100+value* -0.9  + '%';
+    subslogan.style.letterSpacing = value * 0.3 + 'px';
+    btn.style.opacity =   0 + '%';
     viento.style.left = value* -5  + 'px';
     header.style.marginTop = value * 0.4 + 'px';
 })
